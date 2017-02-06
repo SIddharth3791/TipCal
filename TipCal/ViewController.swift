0479a76d66cc261d5772dd72b3530b657ed94143
siddharth <patelsi14@students.ecu.edu>
2017-02-06 12:30:53 -0500

    Implemeneted Tip Calculator to simplify complicated tip process :)

--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+class TADViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var CalculateButton: UIButton!
+    @IBOutlet weak var BillAmountText: UITextField!
+    @IBOutlet weak var TipCalculatedLabel: UILabel!
+    @IBOutlet weak var TipSlider: UISlider!
+    
+    
+    @IBOutlet weak var TipCalculatedByUser: UILabel!
+    @IBOutlet weak var LabelForBill: UILabel!
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+  
+    @IBAction func CalTipSlider(_ sender: UISlider) {
+        
+        let userInput = Float(BillAmountText.text!)
+        //let currentValue = Int(sender.value)
+        if (userInput == nil)
+        {
+            let alertController = UIAlertController(title: "Tip Cal", message:
+                "Bill Amount can not be 0 or less", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }else
+        {
+            
+            TipCalculatedByUser.text = String(self.TipSlider.value)
+            LabelForBill.text = BillAmountText.text
+            
+        }
     }
 
-
+    @IBAction func CalculateButtonPressed(_ sender: Any) {
+        
+        let userBill = Double(BillAmountText.text!)
+        var tip = Double(TipCalculatedByUser.text!)
+        let totalBill = (userBill! * tip!)/100
+        TipCalculatedLabel.text = String(userBill! + totalBill)
+        
+    
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
-
